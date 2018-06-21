ef848e3d2461c36be49aeb4a2e7963d9ec8b835a
Arthur Laheyne <a.laheyne@acat.fr>
2018-06-21 11:56:33 +0200

    account menu

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import AvatarMini from './AvatarMini';
 
 class Login extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      joueur: null,
-      connecting: false
+      joueur: {pseudo: 'arthur'},
+      connecting: false,
+      failed: false,
+      showMenu: false,
     };
   }
 
   componentDidMount() {
-    this.setState({
-      connecting: false
-    });
+    // this.setState({
+    //   connecting: true
+    // });
   }
 
   responseFacebook = (response) => {
+    console.log('test');
     fetch(process.env.REACT_APP_API_URL + '/auth/facebook/token', {
       method: 'post',
       headers: {
@@ -27,31 +31,67 @@ class Login extends Component {
       body: JSON.stringify({access_token: response.accessToken})
     })
       .then(res => {
-        res.json().then((data) => {
-          if (data.joueur) {
-            this.setState({
-              joueur: data.joueur,
-              connecting: false
-            })
-          }
-        });
+        if (!res.ok) {
+        } else {
+          res.json().then((data) => {
+            if (data.joueur) {
+              this.setState({
+                joueur: data.joueur,
+                connecting: false
+              })
+            }
+          });
+        }
+        this.setState({
+          connecting: false
+        })
       });
   }
 
-  logoutCallback = () => {
+  logout = () => {
+    console.log('deco');
     this.setState({
       joueur: null
     })
   }
 
+  login = () => {
+    this.setState({
+      connecting: true
+    })
+  }
+
+  failed = () => {
+    this.setState({
+      connecting: false,
+      failed: true
+    })
+  }
+
+  toggleMenu = () => {
+    console.log('bonjour');
+    this.setState(prevState => ({
+      showMenu: !prevState.showMenu
+    }))
+  }
+
   render() {
     if (this.state.joueur) {
       return (
-        <span onClick={() => this.logoutCallback()} className="facebook">{this.state.joueur.pseudo}</span>
+        <div className="facebook" onClick={this.toggleMenu}>
+          <AvatarMini joueur={this.state.joueur.pseudo} width="30" height="30"/> <span >{this.state.joueur.pseudo} </span>
+          <div id="account-menu" style={{ display: this.state.showMenu ? "block" : "none"}}>
+            <ul>
+              <li onClick={this.logout}>DECONNEXION</li>
+            </ul>
+          </div>
+        </div>
       )
     } else if (this.state.connecting) {
       return (
-        <span className="facebook">connecting...</span>
+        <div className="facebook">
+          <span>connecting...</span>
+        </div>
       )
     }
     return (
@@ -59,9 +99,10 @@ class Login extends Component {
         <FacebookLogin
           appId="2593367260889259"
           autoLoad={true}
+          onFailure={this.failed}
           fields="name,email,picture"
-          textButton="Log FB"
-          onClick={this.onClick}
+          textButton={"Log FB" + (this.state.failed ? "(f)" : "")}
+          onClick={this.login}
           callback={this.responseFacebook}
         />
       </div>
